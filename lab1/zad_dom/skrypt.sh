2fde346d7f5897ae51ac2719996be3c9966cc4b8
Arkadia LIN <arkadiakowalczyk@gmail.com>
2023-03-25 20:08:51 +0100

    iteracja i mv/cp

--- lab1/zad_dom/skrypt.sh
+++ lab1/zad_dom/skrypt.sh
@@ -19,7 +19,21 @@ for FILE in ${ITEMS}; do
     else
         echo "nie znaleziono ${FILE}"
     fi
- 
 done
+
+#wczytaj zawartosc katalogu do listy
+ITEMS=$(echo $(ls ${SOURCE_DIR}))
+
+for FILE in ${ITEMS}; do
+#iteracja po pozostalych plikach w source
+    if [[ -f ${SOURCE_DIR}/${FILE} ]]; then
+        echo "przenosze ${SOURCE_DIR}/${FILE} do ${TARGET_DIR}"
+        mv ${SOURCE_DIR}/${FILE} ${TARGET_DIR}
+    elif [[ -d ${SOURCE_DIR}/${FILE} ]]; then
+        echo "kopiuje directory ${SOURCE_DIR}/${FILE} wraz z zawartoscia do ${TARGET_DIR}"
+        cp -r ${SOURCE_DIR}/${FILE} ${TARGET_DIR}
+    fi
+done
+
 
 
